af00c36de0a23e457edde648a2efa3b7cc39685b
Amit Kumar <amitkumarus14jan@gmail.com>
2020-04-01 23:41:59 +0530

    java file tesing with jenkins

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test {
+	public static void main(String as[]) {
+		System.out.println("Kedar"+"\n"+new kd("kedar","jadhav"));
+	}
+}
+
+class kd{
+	String name;
+	String ls;
+
+	kd(String n, String s){
+
+		name = n;
+		ls = s;
+	}
+
+	public String toString(){
+	System.out.println(name+"\n"+ls);
+	return name + ls;
+}
+}
+
