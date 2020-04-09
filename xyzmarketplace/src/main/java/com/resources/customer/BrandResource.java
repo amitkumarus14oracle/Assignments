bb40b8eb070fce30497f872f105c787124c48acc
Amit Kumar <amitkumarus14jan@gmail.com>
2020-04-09 14:05:45 +0530

    latest changes

---
+++ xyzmarketplace/src/main/java/com/resources/customer/BrandResource.java
@@ -0,0 +1,16 @@
+package com.resources.customer;
+
+import com.model.Product;
+
+import javax.ws.rs.Path;
+import java.util.List;
+
+public class BrandResource {
+
+
+
+    @Path("/brand")
+    public List<Product> getItems() {
+        return null;
+    }
+}
